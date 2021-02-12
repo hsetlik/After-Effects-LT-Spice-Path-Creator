060e82b97934a4f5b1953805b3e696ae8d310607
Hayden Setlik <haydensetlik@gmail.com>
2021-02-12 11:17:41 -0700

    Fixed time scaling

--- ltspice_path_creator.jsx
+++ ltspice_path_creator.jsx
@@ -1,22 +1,26 @@
+//sorting out all the UI dimensions
 var loadButton_pos = [10, 10, 100, 30];
 var fileName_pos = [110, 10, 200, 30];
 var heightLabel_pos = [10, 40, 80, 60];
-var heightSlider_pos = [115, 40, 215, 60];
+var heightSlider_pos = [115, 40, 300, 60];
 var heightEdit_pos = [80, 40, 115, 60];
 var widthLabel_pos = [10, 60, 80, 80];
-var widthSlider_pos = [115, 60, 215, 80];
+var widthSlider_pos = [115, 60, 300, 80];
 var widthEdit_pos = [80, 60, 115, 80];
 var resLabel_pos = [10, 80, 80, 100];
 var resEdit_pos = [80, 80, 115, 100];
-var resSlider_pos = [115, 80, 215, 100];
+var resSlider_pos = [115, 80, 300, 100];
 var startLabel_pos = [10, 100, 80, 120];
 var startEdit_pos = [80, 100, 115, 120];
-var startSlider_pos = [115, 100, 215, 120];
+var startSlider_pos = [115, 100, 300, 120];
 var endLabel_pos = [10, 120, 80, 140];
 var endEdit_pos = [80, 120, 115, 140];
-var endSlider_pos = [115, 120, 215, 140];
+var endSlider_pos = [115, 120, 300, 140];
 
 var createButton_pos = [10, 145, 100, 170];
+var test1_pos = [10, 170, 100, 190];
+var test2_pos = [10, 190, 100, 210];
+var test3_pos = [10, 210, 100, 230];
 
 var fileName;
 var loadButton;
@@ -38,6 +42,7 @@ var endLabel;
 var endEdit;
 var endSlider;
 
+//track how many traces are in the comp for naming
 var numTraces = 0;
 
 var project = app.project;
@@ -62,13 +67,62 @@ function checkAllowed(inputFile)
     }
 }
 
+function closestNum(array, num) {
+    var i = 0;
+    var minDiff = 1000;
+    var ans;
+    for (i in array) {
+      var m = Math.abs(num - array[i]);
+      if (m < minDiff) {
+        minDiff = m;
+        ans = array[i];
+      }
+    }
+    return ans;
+  }
+  //returns the index of the value closest to 'num'
+  function closestIndex(array, num) {
+    var i = 0;
+    var minDiff = 1000;
+    var ans;
+    var bestIndex;
+    for (i in array) {
+      var m = Math.abs(num - array[i]);
+      if (m < minDiff) {
+        minDiff = m;
+        ans = array[i];
+        bestIndex = i;
+      }
+    }
+    return bestIndex;
+  }
+
+
+
+function sciNot(input)
+{
+    var leftHandExp = /(\d|\S)+(?=e)/;
+    var leftHandArr = input.match(leftHandExp); 
+    var leftHandStr = leftHandArr[0];
+    var leftHandNum = parseFloat(leftHandStr);    
+    var rightHandExp = /[^e]*$/g;
+    var rightHandArr = input.match(rightHandExp);
+    var rightHandNum = parseInt(rightHandArr[0]);
+    var exponent = Math.pow(10, rightHandNum);
+
+    var output = leftHandNum * exponent;
+    return output;
+}
+
 function createPanel(thisObj)
 {
     var panel = thisObj;
     loadButton = panel.add("button", loadButton_pos, "Choose a File");
     fileName = panel.add("statictext", fileName_pos, "None Selected");
     createButton = panel.add("button", createButton_pos, "Create Path"); 
+    //disable until a valid file is loaded
     createButton.enabled = false;
+    //tying together the slider and the edittext
     heightLabel = panel.add("statictext", heightLabel_pos, "Height: ");
     heightSlider = panel.add("slider", heightSlider_pos, 1080, 0, 5000);
     heightEdit = panel.add("edittext", heightEdit_pos, heightSlider.value.toString());
@@ -110,57 +164,38 @@ function createPanel(thisObj)
             resSlider.value = inputNum;
         }
     }
-    
-    function closest(array,num){
-        var i=0;
-        var minDiff=1000;
-        var ans;
-        for(i in array){
-             var m=Math.abs(num-array[i]);
-             if(m<minDiff){ 
-                    minDiff=m; 
-                    ans=array[i]; 
-                }
-          }
-        return ans;
-    }
 
     loadButton.onClick = function()
     {
         sourceFile = sourceFile.openDlg("Choose a file", checkAllowed(sourceFile));
         fileName.text = sourceFile.name;
         var openFile = sourceFile.open('r');
+        //store the whole file in a string
         var fileStr = sourceFile.read();
-        var regExp = /(\S)+$/gm;
-        var rawVoltageStrings = fileStr.match(regExp);
+        var leftExp = /(\S)+$/gm;
+        var rightExp = /^\S+/gm;
+        //get all the left-hand column voltages into an array
+        var rawVoltageStrings = fileStr.match(leftExp);
+        var rawTimeStrings = fileStr.match(rightExp)
         var lineNumber = rawVoltageStrings.length;
+        //resSlider.maxvalue = (lineNumber / 2);
+        //start on line 1 so the names of the columns aren't included
         for(var line = 1; line < lineNumber; line++)
         {
-           var leftHand = rawVoltageStrings[line].toString();
-           var leftHandExp = /(\d|\S)+(?=e)/;
-            var leftHandArr = leftHand.match(leftHandExp);
-           
-           var leftHandStr = leftHandArr[0];
-           var leftHandNum = parseFloat(leftHandStr);
-           
-           
-           var rightHand = rawVoltageStrings[line].toString();
-           var rightHandExp = /[^e]*$/g;
-           var rightHandArr = rightHand.match(rightHandExp);
-           var rightHandNum = parseInt(rightHandArr[0]);
-           var exponent = Math.pow(10, rightHandNum);
+           //more RegEx stuff to parse the scientific notation
+           //finding the voltage
 
-           
-           
-           var voltageVal = leftHandNum * exponent;
-           
+           var voltageVal = sciNot(rawVoltageStrings[line].toString());
            rawVoltageNums.push(voltageVal);
+           var timeVal = sciNot(rawTimeStrings[line].toString());
+           rawTimeNums.push(timeVal);
         }
         if(rawVoltageNums.length > 0)
         {
             fileIsLoaded = true;
             createButton.enabled = true;
 
+            //add the start/end time controls
             startLabel = panel.add("statictext", startLabel_pos, "Start: ");
             startSlider = panel.add("slider", startSlider_pos, 0, 0, 100);
             startEdit = panel.add("edittext", startEdit_pos, startSlider.value.toString());
@@ -200,44 +235,48 @@ function createPanel(thisObj)
         if(fileIsLoaded == true)
         {
             var valuesToUse = [];
+            var indecesToUse = [];
             var numDataPoints = resSlider.value;
+            var dataDuration = rawTimeNums[rawTimeNums.length - 1];
+            
+            //check the start/end sliders to figure out what values to pull
             var start = startSlider.value;
-            var startIndex;
-            if(start > 1)
-            {
-                startIndex = Math.floor(rawVoltageNums.length * (start / 100));
-            }
-            else
-            {
-                startIndex = 0;
-            }
             var end = endSlider.value;
-            var endIndex = 0;
-            if(end >= 1)
-            {
-                endIndex = rawVoltageNums.length * (end / 100);
-            }
-            else
-            {
-                alert("End value must be greater than start");
+            var startSecs = (start / 100) * dataDuration;
+            var endSecs = (end / 100) * dataDuration;
+            var windowDuration = endSecs - startSecs;
+            var timeIncrement = windowDuration / numDataPoints;
+            var test3 = panel.add("statictext", test3_pos, timeIncrement.toString());
+            for(i = 0; i < numDataPoints; i++)
+            { 
+              var timePos = startSecs + (timeIncrement * i);
+              if(i == 0)
+              {
+                var test1 = panel.add("statictext", test1_pos, timePos.toString());
+                var array = [3, 7, 4.1, 5.8, 6];
+                var currentIndex = closestIndex(array, 4);
+                //should display '2'
+                var test2 = panel.add("statictext", test2_pos, currentIndex.toString());
+              }
+              indecesToUse.push(closestIndex(rawTimeNums, timePos));
             }
             
-            var increment = Math.floor((endIndex - startIndex) / numDataPoints);
-            var maxVal = 0.0;
+            //picking out values based on resolution and finding the largest one for the vertical scale
+            var maxVoltage = 0.0;
             for(var i = 0; i < numDataPoints; i++)
             {
-                var thisValue = rawVoltageNums[startIndex + (i * increment)];
-                
-                valuesToUse.push(thisValue);
-                if(thisValue > maxVal)
+                var thisVoltage = rawVoltageNums[indecesToUse[i]];
+                valuesToUse.push(thisVoltage);
+                if(Math.abs(thisVoltage) > maxVoltage)
                 {
-                    maxVal = thisValue;
+                    maxVoltage = Math.abs(thisVoltage);
                 }       
             }
             comp.openInViewer();
+            //make a new shape layer
             var shapeLayer = comp.layers.addShape();
             numTraces += 1;
-            var traceCount = numTraces.toString();
+            //determining the layer name
             var namePrefix = "Trace ";
             for(i = 0; i < comp.numLayers; ++i)
             {
@@ -248,32 +287,24 @@ function createPanel(thisObj)
                      var arr = name.match(/\d/g);
                      if(parseInt(arr[0]) >= traceCount)
                      {
-                         traceCount = parseInt(arr[0]);
+                         numTraces = parseInt(arr[0]);
                      }
                  }
             }
+            var traceCount = numTraces.toString();
             var layerName = namePrefix.concat(traceCount);
             shapeLayer.name = layerName;
             //creating all the properties
             var pathGroup = shapeLayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Group");
             var strokeGroup = shapeLayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Stroke");
              //making a shape based on the data
-            var duration = rawTimeNums[rawTimeNums.length - 1];
             var verts = [];
             var inTans = [];
             var outTans = [];
             var xIncrement = (widthSlider.value / numDataPoints);
             var halfY = comp.height / 2;
             var halfX = comp.width / 2;
-            var vMax = 1;
-            for(i = 0; i < numDataPoints; i++)
-            {
-                if(valuesToUse[i] > vMax)
-                {
-                    vMax = valuesToUse[i];
-                }
-            }
-            var gain = (heightSlider.value / vMax) / 2;
+            var gain = (heightSlider.value / maxVoltage) / 2;
             for(var i = 0; i < numDataPoints; i++)
             {
                 var xPos = (xIncrement * i) - halfX;
@@ -291,8 +322,8 @@ function createPanel(thisObj)
             pathShape.vertices = verts;
             pathShape.outTangents = outTans;
             pathShape.closed = false;
+            //assigning that shape to the layer path
             shapeLayer.property("Contents").property(1).property(2).setValue(pathShape);
-            
         }
     }
     return panel;
